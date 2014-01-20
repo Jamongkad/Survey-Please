ebb83f30a3db9a23b05d79c83d27574bb35b74fe
jamongkad <wrm932@gmail.com>
2014-01-20 18:45:46 +0800

    asd

--- public/javascripts/game/services/flashobjects/closet.js
+++ public/javascripts/game/services/flashobjects/closet.js
@@ -7,7 +7,10 @@ angular.module('Closet', [])
         this.desc = "A large wooden closet. It looks quite antique but otherwise kept in good shape. ";
 
         this.open = function() { 
-            $rootScope.$broadcast("push-message", "Nothing seems to be out of place. ");
+            var msg = "The closet is as spartan as the room. You find a plain white tshirt and pajamas neatly folded ." + 
+                      "A few clothes hangers on the closet hanger. " + 
+                      "Apart from that nothing seems out of the ordinary. ";
+            $rootScope.$broadcast("push-message", msg);
         }
 
     }
